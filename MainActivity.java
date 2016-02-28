3cf284b03711bd8508ee38f426241c816fc922b0
pkwiatek6 <pkwiatek6@gmail.com>
2016-02-28 08:32:19 -0500

    Added files via upload

---
+++ MainActivity.java
@@ -0,0 +1,493 @@
+/*
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nerdsquad.note60;
+
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+import com.google.vrtoolkit.cardboard.CardboardView;
+import com.google.vrtoolkit.cardboard.Eye;
+import com.google.vrtoolkit.cardboard.HeadTransform;
+import com.google.vrtoolkit.cardboard.Viewport;
+
+//import android.content.Context;
+import android.content.Intent;
+import android.opengl.GLES20;
+import android.opengl.Matrix;
+import android.os.Bundle;
+//import android.os.Vibrator;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+
+public class MainActivity extends CardboardActivity implements CardboardView.StereoRenderer,
+        RecognitionListener {
+  private static final String TAG = "MainActivity";
+
+  private static final float Z_NEAR = 0.1f;
+  private static final float Z_FAR = 100.0f;
+
+  private static final float CAMERA_Z = 0.01f;
+
+  private static final int COORDS_PER_VERTEX = 3;
+
+  // We keep the light always position just above the user.
+  private static final float[] LIGHT_POS_IN_WORLD_SPACE = new float[] {0.0f, 2.0f, 0.0f, 1.0f};
+
+  private final float[] lightPosInEyeSpace = new float[4];
+
+  private FloatBuffer floorVertices;
+  private FloatBuffer floorColors;
+  private FloatBuffer floorNormals;
+
+  private int floorProgram;
+  private int noteProgram;
+
+  private int floorPositionParam;
+  private int floorNormalParam;
+  private int floorColorParam;
+  private int floorModelParam;
+  private int floorModelViewParam;
+  private int floorModelViewProjectionParam;
+  private int floorLightPosParam;
+
+  private float[] camera;
+  private float[] view;
+  private float[] headView;
+  private float[] modelViewProjection;
+  private float[] modelView;
+  private float[] modelFloor;
+
+  private float[] headForward;
+
+  private float floorDepth = 20f;
+
+  private SpeechRecognizer speech = null;
+  private Intent recognizerIntent;
+  private String LOG_TAG = "VoiceRecognitionActivity";
+
+  private ConcurrentLinkedQueue<Note> notes;
+  private ConcurrentLinkedQueue<float[]> newNotes;
+  //private Vibrator vibrator;
+  private CardboardOverlayView overlayView;
+
+  /**
+   * Converts a raw text file, saved as a resource, into an OpenGL ES shader.
+   *
+   * @param type The type of shader we will be creating.
+   * @param resId The resource ID of the raw text file about to be turned into a shader.
+   * @return The shader object handler.
+   */
+  private int loadGLShader(int type, int resId) {
+    String code = readRawTextFile(resId);
+    int shader = GLES20.glCreateShader(type);
+    GLES20.glShaderSource(shader, code);
+    GLES20.glCompileShader(shader);
+
+    // Get the compilation status.
+    final int[] compileStatus = new int[1];
+    GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
+
+    // If the compilation failed, delete the shader.
+    if (compileStatus[0] == 0) {
+      Log.e(TAG, "Error compiling shader: " + GLES20.glGetShaderInfoLog(shader));
+      GLES20.glDeleteShader(shader);
+      shader = 0;
+    }
+
+    if (shader == 0) {
+      throw new RuntimeException("Error creating shader.");
+    }
+
+    return shader;
+  }
+
+  /**
+   * Checks if we've had an error inside of OpenGL ES, and if so what that error is.
+   *
+   * @param label Label to report in case of error.
+   */
+  private static void checkGLError(String label) {
+    int error;
+    while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
+      Log.e(TAG, label + ": glError " + error);
+      throw new RuntimeException(label + ": glError " + error);
+    }
+  }
+
+  /**
+   * Sets the view to our CardboardView and initializes the transformation matrices we will use
+   * to render our scene.
+   */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.common_ui);
+    CardboardView cardboardView = (CardboardView) findViewById(R.id.cardboard_view);
+    cardboardView.setRestoreGLStateEnabled(false);
+    cardboardView.setRenderer(this);
+    setCardboardView(cardboardView);
+
+    speech = SpeechRecognizer.createSpeechRecognizer(this);
+    speech.setRecognitionListener(this);
+    recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+    recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE,
+            "en");
+    recognizerIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,
+            this.getPackageName());
+    recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+    recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 10);
+
+    notes = new ConcurrentLinkedQueue<Note>();
+    newNotes = new ConcurrentLinkedQueue<float[]>();
+    camera = new float[16];
+    view = new float[16];
+    modelViewProjection = new float[16];
+    modelView = new float[16];
+    modelFloor = new float[16];
+    headForward = new float[3];
+    headView = new float[16];
+    //vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+    overlayView = (CardboardOverlayView) findViewById(R.id.overlay);
+    //overlayView.show3DToast("Pull the magnet when you find an object.");
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    if (speech != null) {
+      speech.destroy();
+      Log.i(LOG_TAG, "destroy");
+    }
+  }
+  @Override
+  public void onBeginningOfSpeech() {
+    Log.i(LOG_TAG, "onBeginningOfSpeech");
+  }
+
+  @Override
+  public void onBufferReceived(byte[] buffer) {
+    Log.i(LOG_TAG, "onBufferReceived: " + buffer);
+  }
+
+  @Override
+  public void onEndOfSpeech() {
+    Log.i(LOG_TAG, "onEndOfSpeech");
+  }
+  @Override
+  public void onResume() {
+    super.onResume();
+  }
+  @Override
+  public void onError(int errorCode) {
+    String errorMessage = getErrorText(errorCode);
+    Log.d(LOG_TAG, "FAILED " + errorMessage);
+  }
+
+  @Override
+  public void onEvent(int arg0, Bundle arg1) {
+    Log.i(LOG_TAG, "onEvent");
+  }
+
+  @Override
+  public void onPartialResults(Bundle arg0) {
+    Log.i(LOG_TAG, "onPartialResults");
+  }
+
+  @Override
+  public void onReadyForSpeech(Bundle arg0) {
+    Log.i(LOG_TAG, "onReadyForSpeech");
+  }
+
+  @Override
+  public void onResults(Bundle results) {
+    //get text from speech here
+    Log.i(LOG_TAG, "onResults");
+    ArrayList<String> matches = results
+            .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+    String text = "";
+    text += matches.get(0) + "\n";
+  }
+  @Override
+  public void onRmsChanged(float rmsdB) {
+    Log.i(LOG_TAG, "onRmsChanged: " + rmsdB);
+  }
+  public static String getErrorText(int errorCode) {
+    String message;
+    switch (errorCode) {
+      case SpeechRecognizer.ERROR_AUDIO:
+        message = "Audio recording error";
+        break;
+      case SpeechRecognizer.ERROR_CLIENT:
+        message = "Client side error";
+        break;
+      case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+        message = "Insufficient permissions";
+        break;
+      case SpeechRecognizer.ERROR_NETWORK:
+        message = "Network error";
+        break;
+      case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+        message = "Network timeout";
+        break;
+      case SpeechRecognizer.ERROR_NO_MATCH:
+        message = "No match";
+        break;
+      case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+        message = "RecognitionService busy";
+        break;
+      case SpeechRecognizer.ERROR_SERVER:
+        message = "error from server";
+        break;
+      case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+        message = "No speech input";
+        break;
+      default:
+        message = "Didn't understand, please try again.";
+        break;
+    }
+    return message;
+  }
+  @Override
+  public void onRendererShutdown() {
+    Log.i(TAG, "onRendererShutdown");
+  }
+
+  @Override
+  public void onSurfaceChanged(int width, int height) {
+    Log.i(TAG, "onSurfaceChanged");
+  }
+
+  /**
+   * Creates the buffers we use to store information about the 3D world.
+   *
+   * <p>OpenGL doesn't use Java arrays, but rather needs data in a format it can understand.
+   * Hence we use ByteBuffers.
+   *
+   * @param config The EGL configuration used when creating the surface.
+   */
+  @Override
+  public void onSurfaceCreated(EGLConfig config) {
+    Log.i(TAG, "onSurfaceCreated");
+    GLES20.glClearColor(0.1f, 0.1f, 0.1f, 0.5f); // Dark background so text shows up well.
+
+
+    // make a floor
+    ByteBuffer bbFloorVertices = ByteBuffer.allocateDirect(WorldLayoutData.FLOOR_COORDS.length * 4);
+    bbFloorVertices.order(ByteOrder.nativeOrder());
+    floorVertices = bbFloorVertices.asFloatBuffer();
+    floorVertices.put(WorldLayoutData.FLOOR_COORDS);
+    floorVertices.position(0);
+
+    ByteBuffer bbFloorNormals = ByteBuffer.allocateDirect(WorldLayoutData.FLOOR_NORMALS.length * 4);
+    bbFloorNormals.order(ByteOrder.nativeOrder());
+    floorNormals = bbFloorNormals.asFloatBuffer();
+    floorNormals.put(WorldLayoutData.FLOOR_NORMALS);
+    floorNormals.position(0);
+
+    ByteBuffer bbFloorColors = ByteBuffer.allocateDirect(WorldLayoutData.FLOOR_COLORS.length * 4);
+    bbFloorColors.order(ByteOrder.nativeOrder());
+    floorColors = bbFloorColors.asFloatBuffer();
+    floorColors.put(WorldLayoutData.FLOOR_COLORS);
+    floorColors.position(0);
+
+    int vertexShader = loadGLShader(GLES20.GL_VERTEX_SHADER, R.raw.light_vertex);
+    int gridShader = loadGLShader(GLES20.GL_FRAGMENT_SHADER, R.raw.grid_fragment);
+    int passthroughShader = loadGLShader(GLES20.GL_FRAGMENT_SHADER, R.raw.passthrough_fragment);
+
+    noteProgram = GLES20.glCreateProgram();
+    GLES20.glAttachShader(noteProgram, vertexShader);
+    GLES20.glAttachShader(noteProgram, passthroughShader);
+    GLES20.glLinkProgram(noteProgram);
+    GLES20.glUseProgram(noteProgram);
+
+    floorProgram = GLES20.glCreateProgram();
+    GLES20.glAttachShader(floorProgram, vertexShader);
+    GLES20.glAttachShader(floorProgram, gridShader);
+    GLES20.glLinkProgram(floorProgram);
+    GLES20.glUseProgram(floorProgram);
+
+    checkGLError("Floor program");
+
+    floorModelParam = GLES20.glGetUniformLocation(floorProgram, "u_Model");
+    floorModelViewParam = GLES20.glGetUniformLocation(floorProgram, "u_MVMatrix");
+    floorModelViewProjectionParam = GLES20.glGetUniformLocation(floorProgram, "u_MVP");
+    floorLightPosParam = GLES20.glGetUniformLocation(floorProgram, "u_LightPos");
+
+    floorPositionParam = GLES20.glGetAttribLocation(floorProgram, "a_Position");
+    floorNormalParam = GLES20.glGetAttribLocation(floorProgram, "a_Normal");
+    floorColorParam = GLES20.glGetAttribLocation(floorProgram, "a_Color");
+
+    GLES20.glEnableVertexAttribArray(floorPositionParam);
+    GLES20.glEnableVertexAttribArray(floorNormalParam);
+    GLES20.glEnableVertexAttribArray(floorColorParam);
+
+    checkGLError("Floor program params");
+
+    Matrix.setIdentityM(modelFloor, 0);
+    Matrix.translateM(modelFloor, 0, 0, -floorDepth, 0); // Floor appears below user.
+
+    /*
+    float R = -3.5f;
+    for (int i = 0; i < 5; i++) {
+      float x = (float) Math.sin((float)i*2.0f*Math.PI / 5.0f) * R;
+      float z = (float) Math.cos((float) i * 2.0f * Math.PI / 5.0f) * R;
+      notes.add(new Note("", noteProgram, x, 0.0f, z));
+    }*/
+
+    checkGLError("onSurfaceCreated");
+  }
+
+  /**
+   * Converts a raw text file into a string.
+   *
+   * @param resId The resource ID of the raw text file about to be turned into a shader.
+   * @return The context of the text file, or null in case of error.
+   */
+  private String readRawTextFile(int resId) {
+    InputStream inputStream = getResources().openRawResource(resId);
+    try {
+      BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+      StringBuilder sb = new StringBuilder();
+      String line;
+      while ((line = reader.readLine()) != null) {
+        sb.append(line).append("\n");
+      }
+      reader.close();
+      return sb.toString();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+  /**
+   * Prepares OpenGL ES before we draw a frame.
+   *
+   * @param headTransform The head transformation in the new frame.
+   */
+  @Override
+  public void onNewFrame(HeadTransform headTransform) {
+    //add all the new notes the next frame
+    while (!newNotes.isEmpty()) {
+      float[] newNote = newNotes.poll();
+      notes.add(new Note("", noteProgram, newNote[0], newNote[1], newNote[2]));
+    }
+    // Build the camera matrix and apply it to the ModelView.
+    Matrix.setLookAtM(camera, 0, 0.0f, 0.0f, CAMERA_Z, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+
+    headTransform.getHeadView(headView, 0);
+
+    headTransform.getForwardVector(headForward, 0);
+
+    checkGLError("onReadyToDraw");
+  }
+
+  /**
+   * Draws a frame for an eye.
+   *
+   * @param eye The eye to render. Includes all required transformations.
+   */
+  @Override
+  public void onDrawEye(Eye eye) {
+    GLES20.glEnable(GLES20.GL_DEPTH_TEST);
+    GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+    GLES20.glEnable(GLES20.GL_CULL_FACE);
+    GLES20.glCullFace(GLES20.GL_BACK);
+
+    checkGLError("colorParam");
+
+    // Apply the eye transformation to the camera.
+    Matrix.multiplyMM(view, 0, eye.getEyeView(), 0, camera, 0);
+
+    // Set the position of the light
+    Matrix.multiplyMV(lightPosInEyeSpace, 0, view, 0, LIGHT_POS_IN_WORLD_SPACE, 0);
+
+    float[] perspective = eye.getPerspective(Z_NEAR, Z_FAR);
+
+    checkGLError("Before drawing notes");
+    int i = 0;
+    for (Note o : notes) {
+      o.drawNote(view, perspective, lightPosInEyeSpace);
+      i++;
+      checkGLError("drew a note " + i);
+    }
+    checkGLError("After drawing notes");
+
+    // Set modelView for the floor, so we draw floor in the correct location
+    Matrix.multiplyMM(modelView, 0, view, 0, modelFloor, 0);
+    checkGLError("Multiplied once");
+    Matrix.multiplyMM(modelViewProjection, 0, perspective, 0, modelView, 0);
+    checkGLError("Before drawing floor");
+    drawFloor();
+  }
+
+  @Override
+  public void onFinishFrame(Viewport viewport) {}
+
+  /**
+   * Draw the floor.
+   *
+   * <p>This feeds in data for the floor into the shader. Note that this doesn't feed in data about
+   * position of the light, so if we rewrite our code to draw the floor first, the lighting might
+   * look strange.
+   */
+  public void drawFloor() {
+    GLES20.glUseProgram(floorProgram);
+
+    // Set ModelView, MVP, position, normals, and color.
+    GLES20.glUniform3fv(floorLightPosParam, 1, lightPosInEyeSpace, 0);
+    GLES20.glUniformMatrix4fv(floorModelParam, 1, false, modelFloor, 0);
+    GLES20.glUniformMatrix4fv(floorModelViewParam, 1, false, modelView, 0);
+    GLES20.glUniformMatrix4fv(floorModelViewProjectionParam, 1, false, modelViewProjection, 0);
+    GLES20.glVertexAttribPointer(
+            floorPositionParam, COORDS_PER_VERTEX, GLES20.GL_FLOAT, false, 0, floorVertices);
+    GLES20.glVertexAttribPointer(floorNormalParam, 3, GLES20.GL_FLOAT, false, 0, floorNormals);
+    GLES20.glVertexAttribPointer(floorColorParam, 4, GLES20.GL_FLOAT, false, 0, floorColors);
+
+    GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 6);
+
+    checkGLError("drawing floor");
+  }
+
+  /**
+   * Called when the Cardboard trigger is pulled.
+   */
+  @Override
+  public void onCardboardTrigger() {
+    float newNote[] = new float[3];
+    float distance = 3.5f;
+    newNote[0] = headForward[0] * distance;
+    newNote[1] = headForward[1] * distance;
+    newNote[2] = headForward[2] * distance;
+    newNotes.add(newNote);
+  }
+}
